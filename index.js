6b604e4cd5a86f5e7947ba951bc7b3d990919a23
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-28 05:41:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+  return a+b
+}
+
+function subtracts(a,b){
+  return a-b
+}
+
+function multiplies(a,b){
+  return a*b
+}
+
+function divides(a,b){
+  return a/b
+}
+
+function inc(n){
+  return n+=1
+}
+
+function dec(n){
+  return n-=1
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n,10)
+}
